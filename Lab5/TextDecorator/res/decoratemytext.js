2f89958cc77b23de0edb5a98afba42ac73800380
M.Saleh <mega_unknown@hotmail.com>
2020-05-26 20:22:04 -0500

    Add comments to JS file

--- Lab5/TextDecorator/res/decoratemytext.js
+++ Lab5/TextDecorator/res/decoratemytext.js
@@ -1,30 +1,48 @@
-
-function changeTextDecoration()
-{
-    var textAreaInput = document.getElementById("textDecoration");
-    textAreaInput.style.fontSize = "24pt";
+/** 
+    Change the Font of TextArea
+*/
+function changeTextDecoration() {
+    const textAreaInput = document.getElementById("textDecoration");
+    /*
+    https://stackoverflow.com/questions/15195209/how-to-get-font-size-in-html
+    */
+    const style = window.getComputedStyle(textAreaInput).fontSize;
+    var currentFontSize = parseFloat(style);
+    currentFontSize += 2;
+    textAreaInput.style.fontSize = currentFontSize + "pt";
 }
-function addStyling()
-{
-    var checkBox = document.getElementById("chk");
+
+/** 
+ * Call changeTextDecoration every 500 ms
+ * 
+*/
+function timerFun() {
+    setInterval(changeTextDecoration, 500);
+}
+
+/**
+ * CheckBox function to change the styling 
+ * of the TextArea..
+ */
+function addStyling() {
+    const checkBox = document.getElementById("chk");
     var textAreaInput = document.getElementById("textDecoration");
-    
-    if(checkBox.checked == true)
-    {
+
+    if (checkBox.checked == true) {
         textAreaInput.style.fontWeight = "bold";
         textAreaInput.style.color = "green";
         textAreaInput.style.textDecoration = "underline";
     }
-    else
-    {
+    else {
         textAreaInput.style.fontWeight = "normal";
         textAreaInput.style.color = "black";
         textAreaInput.style.textDecoration = "none";
     }
-        
 }
 
-function alertFun()
-{
+/**
+ * Alert Funcion
+ */
+function alertFun() {
     alert("Hello, world!");
 }
