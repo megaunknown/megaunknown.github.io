8bfd9aee552dcf46337899da6021a9fad5d2a30d
M.Saleh <mega_unknown@hotmail.com>
2020-06-01 18:58:57 -0500

    Add First Part Lab8

---
+++ Lab8/res/acc.js
@@ -0,0 +1,64 @@
+"use strict";
+const Module=(
+    function()
+    {
+        let accountInfoList = [];
+        let txtArea, btnCreateNewAccount,txtAccountName, txtDeposit;
+ 
+        class Account{
+            constructor(_accName,_accDeposit)
+            {
+                this.accName = _accName ;
+                this.accDeposit = _accDeposit; 
+            }
+
+            getName(){
+                return this.accName;
+            }
+
+            getDeposit(){
+                return this.accDeposit;
+            }
+        }
+
+        
+        let makeAccount = function() {
+            txtAccountName =  document.getElementById('txtAccountName');
+            txtDeposit =  document.getElementById('txtDeposit');
+            function addNewAccount() {
+                let cls = new  Account(txtAccountName,txtDeposit);
+                return cls;
+            }
+        
+            return { 
+                addAccount: addNewAccount
+            };
+        }
+        
+        window.onload =  function(){
+            btnCreateNewAccount =  this.document.getElementById('btnCreateNewAccount');
+            txtArea  =  document.getElementById('txtArea');
+            btnCreateNewAccount.onclick = onButtonAddClick;
+        }
+        
+        function onButtonAddClick()
+        {
+            //Clear the text
+            txtArea.value = '';
+            let acc = new makeAccount();
+            //get the object
+            var data = acc.addAccount();
+            //push the object to the array
+            accountInfoList.push(data);
+        
+            let strText = txtArea.value;
+            //traverse through the array and add them to the text.
+            for (const obj of accountInfoList) {
+               strText += 'Account name: ' + obj.accountName + '\t' + 'Balance: ' + obj.balance + '\n'; 
+            }
+            // view the objects on the textarea.
+            txtArea.value = strText;
+        }
+
+    }
+)();
