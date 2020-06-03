b264a2a1d2c313dcaa049476fb54cabee25c8823
M.Saleh <mega_unknown@hotmail.com>
2020-06-03 17:16:01 -0500

    Fix JSON

--- Lab10/res/json.js
+++ Lab10/res/json.js
@@ -3,14 +3,27 @@ $(function(){
 
     $("#btnGetUserInfo").click(function(){
         let strUserId = $("#txtUserId").val();
-        /*
+        alert(strUserId);
+        /**/
         JSONString = fetch('https://jsonplaceholder.typicode.com/users/' + strUserId )
                 .then(response => response.json())
                 .then(json => console.log(json));
-
-        JSONObject = JSON.parse(JSONString);
-        */
-        $('#textareaOutput').val("sadfsafsdf");
+               
+        JSONObject =  JSON.parse(JSON.stringify(JSONString));
+        
+       $('#textareaOutput').val("");
+    //    $('#textareaOutput').val("sadfsafsdf");
+        let address = JSONObject.address;
+        /* */
+        let strUserInfo = `Name:${ JSONObject.name}\n
+                           UserName:${JSONObject.username}\n
+                           Email:${JSONObject.email}\n
+                           Address:${address.street + ',' + address.suite }\n
+                           Phone:${JSONObject.phone}\n
+                           Website:${JSONObject.website}`;
+       
+       //let strUserInfo = "Name:" + JSONObject.name;
+        $('#textareaOutput').val(strUserInfo);
     });
     /*
     $("#btnGetPosts").click(function(){
