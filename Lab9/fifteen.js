1262288e3c21344cbcde1756e526094cb46bf8d3
M.Saleh <mega_unknown@hotmail.com>
2020-06-03 22:51:18 -0500

    Lab 9 - Swapping done, mousehover done

--- Lab9/fifteen.js
+++ Lab9/fifteen.js
@@ -1,14 +1,14 @@
-init = function() {
+init = function () {
     var puzzleArea = document.getElementById('puzzlearea');
     var divs = puzzleArea.getElementsByTagName("div");
-      
+
     // initialize each piece
-    for (var i=0; i< divs.length; i++) {
+    for (var i = 0; i < divs.length; i++) {
         var div = divs[i];
-        
+
         // calculate x and y for this piece
-        var x = ((i % 4) * 100) ;
-        var y = (Math.floor(i / 4) * 100) ;
+        var x = ((i % 4) * 100);
+        var y = (Math.floor(i / 4) * 100);
 
         // set basic style and background
         div.className = "puzzlepiece";
@@ -16,13 +16,81 @@ init = function() {
         div.style.top = y + 'px';
         div.style.backgroundImage = 'url("background.jpg")';
         div.style.backgroundPosition = -x + 'px ' + (-y) + 'px';
-        
+
         // store x and y for later
         div.x = x;
-        div.y = y; 
-    }        
+        div.y = y;
+    }
+
+    $("#shufflebutton").click(function () {
+        alert("Shuffle...");
+    })
+
+
+    $("#puzzlearea").children().click(function () {
+        alert(this.innerText);
+        let nextDivToCurrent = $(this).next();
+
+        if (nextDivToCurrent.text() == 'X') {
+            let currentDivTxt = $(this).text();
+            let nextDivTxt = $(this).next().text();
+        //    alert("Before swapping" + currentDivTxt + ' ' + nextDivTxt);
+            $(this).text(nextDivTxt);
+            $(this).next().text(currentDivTxt);
+        //    alert("After " + currentDivTxt + ' ' + nextDivTxt);
+           
+        }
+    });
+
+    /*
+        MouseOver 
+    */
+    $("#puzzlearea").children().mouseover(function () {
+        let nextDivToCurrent = $(this).next();
+        if (nextDivToCurrent.text() == 'X') {
+            $(this).addClass("movablepiece");
+        }
+        else {
+            $(this).removeClass("movablepiece");
+        }
+    });
+
+    /*
+    .click(function(){
+        alert(this.innerText);
+        alert("check next " + this.next().text("dd"));
+        //if()
+    })
+    */
+    /*
+        $("#puzzlearea").children().click(function(){
+            alert(this.innerText);
+        })
+    */
+    $("#checkbutton").click(function () {
+        if (checkSolution($("#puzzlearea").children()) == true) {
+            alert("True");
+        }
+        else {
+            alert("false");
+        }
+    });
+
+    function checkAround(element) {
+
+    }
+    function checkSolution(list) {
+        for (let i = 1; i <= 15; i++) {
+            if (list[i - 1].innerText == i.toString(10)) {
+                return false;
+            }
+        }
+        return true;
+    }
 };
 init();
+
+
 
 
 
