c82a4bf656a44aee1632a84793fcbbba0322f317
M.Saleh <mega_unknown@hotmail.com>
2020-06-04 09:28:44 -0500

    FIX JSON Issue

--- Lab10/res/json.js
+++ Lab10/res/json.js
@@ -1,48 +1,69 @@
 $(function () {
+
+    $("#btn1").click(function(){
+        $.ajax("https://jsonplaceholder.typicode.com/posts?userId="+  $("#txtUserId").val(),{
+            "type":"GET"
+        }).done(success).fail(failed);
+
+        function success(data){
+        //    console.log(data);
+            let obj = JSON.parse(JSON.stringify(data));
+            console.log(obj);
+        }
+
+        function failed(){
+            console.log("Failed");
+        }
+
+    });
+
     $("#btnGetUserInfo").click(function () {
-        let JSONObject;
-        let strUserId = $("#txtUserId").val();
-        $.ajax('https://jsonplaceholder.typicode.com/users/' + strUserId,
-            {
-                "type": "GET"
-            }).done(success).fail(fail);
-            
-        function success(data) {
-            JSONObject = JSON.parse(JSON.stringify(data));
-            return JSONObject;
+        $.ajax('https://jsonplaceholder.typicode.com/users/' +  $("#txtUserId").val(),{
+            "type":"GET"
+        }).done(success).fail(failed);
+
+        function success(data){
+            let JSONObject = JSON.parse(JSON.stringify(data));
+            $('#textareaOutput').val("");
+            let address = JSONObject.address;
+            let strUserInfo = `Name:${JSONObject.name}\nUserName:${JSONObject.username}\nEmail:${JSONObject.email}\nAddress:${address.street + ',' + address.suite}\nPhone:${JSONObject.phone}\nWebsite:${JSONObject.website}`;
+            $('#textareaOutput').val(strUserInfo);
         }
 
-        function fail() {
-            console.log("Failed.");
+        function failed(){
+            console.log("Failed");
         }
-
-
-        $('#textareaOutput').val("");
-        let address = JSONObject.address;
-
-        let strUserInfo = `Name:${JSONObject.name}\n
-                           UserName:${JSONObject.username}\n
-                           Email:${JSONObject.email}\n
-                           Address:${address.street + ',' + address.suite}\n
-                           Phone:${JSONObject.phone}\n
-                           Website:${JSONObject.website}`;
-
-        //let strUserInfo = "Name:" + JSONObject.name;
-        //   alert(strUserInfo);
-        $('#textareaOutput').val(strUserInfo);
-
     });
-    /*
+
+  
     $("#btnGetPosts").click(function(){
-        let strUserId = $("#txtUserId").val();
+         $.ajax("https://jsonplaceholder.typicode.com/posts?userId="  +  $("#txtUserId").val(),{
+            "type":"GET"
+        }).done(success).fail(failed);
 
-        JSONString = fetch('https://jsonplaceholder.typicode.com/posts?userId=' + strUserId )
-                .then(response => response.json())
-                .then(json => console.log(json));
-        JSONObject = JSON.parse(JSONString);
+        function success(data){
+            let JSONObject = JSON.parse(JSON.stringify(data));
+            console.log(JSONObject);
+            let strUserPosts = '';
+            
+            for(let i = 0 ; i < JSONObject.length; i++)
+            {
+                var obj = JSONObject[i];
+                var post = `Title::${obj.title}\nBody::${obj.body}`;
+                strUserPosts = strUserPosts + post +'\n';
+            }
+
+            
+            
+            $('#textareaOutput').val(strUserPosts);
+        }
+
+        function failed(){
+            console.log("Failed");
+        }
     });
 
-
+  /*
     $("#btnGetComments").click(function(){
         let strPostId = $("#txtPostId"). val();
         JSONString = fetch('https://jsonplaceholder.typicode.com/comments?postid=' + strPostId )
